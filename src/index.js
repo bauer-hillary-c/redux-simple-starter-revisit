996de667c9d73177a04d0bb87a30355d81494e95
bauer-hillary-c <hbauer@ezcater.com>
2018-02-26 22:25:33 -0500

    adds and wires up input component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import  ReactDOM from "react-dom";
 
+import SearchBar from  "./components/search_bar";
+
 const API_KEY = 'AIzaSyD9TNdVPFKQLxHodTgzNxuWLeArUPcK8RM';
 
 const App = () => {
-  return <div>Hai</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 ReactDOM.render(
